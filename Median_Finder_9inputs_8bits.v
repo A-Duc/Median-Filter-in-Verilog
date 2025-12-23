3bd5bfb70241c4187e5a54b5fd246f7a66c3418c
A-Duc <23520304@gm.uit.edu.vn>
2025-12-23 13:03:24 +0700

    Pipeline Median_Finder

--- Median_Finder_9inputs_8bits.v
+++ Median_Finder_9inputs_8bits.v
@@ -1,55 +1,96 @@
-`include "Ascending_Sorter_3inputs_8bits.v"
+//`include "Ascending_Sorter_3inputs_8bits.v"
 
 module Median_Finder_9inputs_8bits(
     output wire [7:0] median_pixel,
     input  wire [7:0] pixel0, pixel1, pixel2,
     input  wire [7:0] pixel3, pixel4, pixel5,
-    input  wire [7:0] pixel6, pixel7, pixel8
+    input  wire [7:0] pixel6, pixel7, pixel8,
+    input  wire       clk
 );
 
-    wire [7:0] min0, mid0, max0;
-    wire [7:0] min1, mid1, max1;
-    wire [7:0] min2, mid2, max2;
+    reg     [7:0] rmin0, rmid0, rmax0;
+    reg     [7:0] rmin1, rmid1, rmax1;
+    reg     [7:0] rmin2, rmid2, rmax2;
+    
+    wire    [7:0] wmin0, wmid0, wmax0;
+    wire    [7:0] wmin1, wmid1, wmax1;
+    wire    [7:0] wmin2, wmid2, wmax2;
 
-    wire [7:0] max_col0, mid_col1, min_col2;
+    reg     [7:0] rmax_col0, rmid_col1, rmin_col2;
+    wire    [7:0] wmax_col0, wmid_col1, wmin_col2;
 
+    //STAGE 0
     Ascending_Sorter_3inputs_8bits Row0(
-        .min(min0), .mid(mid0), .max(max0),
-        .in0(pixel0), .in1(pixel1), .in2(pixel2)
+        .min(wmin0), .mid(wmid0), .max(wmax0),
+        .in0(pixel0), .in1(pixel1), .in2(pixel2),
+        .clk(clk)
     );
 
     Ascending_Sorter_3inputs_8bits Row1(
-        .min(min1), .mid(mid1), .max(max1),
-        .in0(pixel3), .in1(pixel4), .in2(pixel5)
+        .min(wmin1), .mid(wmid1), .max(wmax1),
+        .in0(pixel3), .in1(pixel4), .in2(pixel5),
+        .clk(clk)
     );
 
     Ascending_Sorter_3inputs_8bits Row2(
-        .min(min2), .mid(mid2), .max(max2),
-        .in0(pixel6), .in1(pixel7), .in2(pixel8)
+        .min(wmin2), .mid(wmid2), .max(wmax2),
+        .in0(pixel6), .in1(pixel7), .in2(pixel8),
+        .clk(clk)
     );
 
-    
 
+    //STAGE 1
     Ascending_Sorter_3inputs_8bits FindMax(
-        .max(max_col0),
-        .in0(min0), .in1(min1), .in2(min2)
+        .max(wmax_col0),
+        .in0(rmin0), .in1(rmin1), .in2(rmin2),        
+        .clk(clk)
     );
 
     Ascending_Sorter_3inputs_8bits FindMid(
-        .mid(mid_col1),
-        .in0(mid0), .in1(mid1), .in2(mid2)
+        .mid(wmid_col1),
+        .in0(rmid0), .in1(rmid1), .in2(rmid2),        
+        .clk(clk)
     );
 
     Ascending_Sorter_3inputs_8bits FindMin(
-        .min(min_col2),
-        .in0(max0), .in1(max1), .in2(max2)
+        .min(wmin_col2),
+        .in0(rmax0), .in1(rmax1), .in2(rmax2),        
+        .clk(clk)
     );
 
+    always @(posedge clk) begin
+        rmin0 <= wmin0;
+        rmid0 <= wmid0;
+        rmax0 <= wmax0;
+
+        rmin1 <= wmin1;
+        rmid1 <= wmid1;
+        rmax1 <= wmax1;
+
+        rmin2 <= wmin2;
+        rmid2 <= wmid2;
+        rmax2 <= wmax2;
+
+        rmax_col0 <= wmax_col0;
+        rmid_col1 <= wmid_col1;
+        rmin_col2 <= wmin_col2;
+    end
+
+    
+    
+    // assign max_col0 = (rmin0 > rmin1) ? ((rmin0 > rmin2) ? rmin0 : rmin2) : ((rmin1 > rmin2) ? rmin1 : rmin2);
+
+    // assign mid_col1 = (rmid0 > rmid1) ? 
+    //              ((rmid1 > rmid2) ? rmid1 : ((rmid0 > rmid2) ? rmid2 : rmid0)) : 
+    //              ((rmid0 > rmid2) ? rmid0 : ((rmid1 > rmid2) ? rmid2 : rmid1));
+        
+    // assign min_col2 = (rmax0 < rmax1) ? ((rmax0 < rmax2) ? rmax0 : rmax2) : ((rmax1 < rmax2) ? rmax1 : rmax2);
 
 
     Ascending_Sorter_3inputs_8bits FindMedian(
         .mid(median_pixel),
-        .in0(max_col0), .in1(mid_col1), .in2(min_col2)
+        .in0(rmax_col0), .in1(rmid_col1), .in2(rmin_col2),
+        .clk(clk)
     );
 
 endmodule
--- tb_Median_Finder_9inputs_8bits.v
+++ tb_Median_Finder_9inputs_8bits.v
@@ -5,14 +5,22 @@ module tb_Median_Finder_9inputs_8bits;
     reg     [7:0] pixel0, pixel1, pixel2;
     reg     [7:0] pixel3, pixel4, pixel5;
     reg     [7:0] pixel6, pixel7, pixel8;
+    reg           clk;
     wire    [7:0] median_pixel;
 
     Median_Finder_9inputs_8bits utt(
         .median_pixel(median_pixel),
         .pixel0(pixel0), .pixel1(pixel1), .pixel2(pixel2), 
         .pixel3(pixel3), .pixel4(pixel4), .pixel5(pixel5), 
-        .pixel6(pixel6), .pixel7(pixel7), .pixel8(pixel8) 
+        .pixel6(pixel6), .pixel7(pixel7), .pixel8(pixel8),
+        .clk(clk)
     );
+
+    initial begin
+        clk = 0;
+    end
+
+    always #5 clk = ~clk;
 
     initial begin
         $dumpfile("tb_Median_Finder_9inputs_8bits.vcd");
@@ -41,9 +49,35 @@ module tb_Median_Finder_9inputs_8bits;
         #10
         pixel0 = 8'd18; pixel1 = 8'd20; pixel2 = 8'd3;
         pixel3 = 8'd12; pixel4 = 8'd12; pixel5 = 8'd6;
-        pixel6 = 8'd15; pixel8 = 8'd12; pixel8 = 8'd9;
+        pixel6 = 8'd15; pixel7 = 8'd12; pixel8 = 8'd9;
 
-        #10;
+        #10
+        pixel0 = 8'd50; pixel1 = 8'd100; pixel2 = 8'd150;
+        pixel3 = 8'd40; pixel4 = 8'd90; pixel5 = 8'd140;
+        pixel6 = 8'd30; pixel7 = 8'd80; pixel8 = 8'd130;
+
+        #10
+        pixel0 = 8'd255; pixel1 = 8'd200; pixel2 = 8'd180;
+        pixel3 = 8'd170; pixel4 = 8'd160; pixel5 = 8'd150;
+        pixel6 = 8'd100; pixel7 = 8'd50; pixel8 = 8'd0;
+
+        #10
+        pixel0 = 8'd64; pixel1 = 8'd32; pixel2 = 8'd16;
+        pixel3 = 8'd128; pixel4 = 8'd64; pixel5 = 8'd32;
+        pixel6 = 8'd8; pixel7 = 8'd4; pixel8 = 8'd2;
+
+        #10
+        pixel0 = 8'd111; pixel1 = 8'd111; pixel2 = 8'd222;
+        pixel3 = 8'd222; pixel4 = 8'd111; pixel5 = 8'd222;
+        pixel6 = 8'd111; pixel7 = 8'd222; pixel8 = 8'd111;
+
+        #10
+        pixel0 = 8'd5; pixel1 = 8'd95; pixel2 = 8'd45;
+        pixel3 = 8'd75; pixel4 = 8'd25; pixel5 = 8'd85;
+        pixel6 = 8'd55; pixel7 = 8'd65; pixel8 = 8'd35;
+
+        #50;
+        $finish;
     end
 
 endmodule
