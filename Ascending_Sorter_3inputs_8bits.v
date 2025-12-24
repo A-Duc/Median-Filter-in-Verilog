18e5d43e49a70c5e9002c19e0263765f7b0008bf
A-Duc <23520304@gm.uit.edu.vn>
2025-12-24 22:09:51 +0700

    Fix sorting bugs

--- Ascending_Sorter_3inputs_8bits.v
+++ Ascending_Sorter_3inputs_8bits.v
@@ -8,58 +8,50 @@ module Ascending_Sorter_3inputs_8bits(min, mid, max,
     input   wire [7:0]  in0, in1, in2;
     output  reg  [7:0]  min, mid, max;
 
-    reg [7:0] rin0, rin1, rin2;
-    reg [7:0] cmp0, cmp1, cmp2;  //0: in0 < in1?; 1:in1 < in2?; in0 < in2? 
+    //reg     [7:0] rin0, rin1, rin2;
+    wire cmp0, cmp1, cmp2;  //0: in0 < in1?; 1:in1 < in2?; in0 < in2? 
+
+    assign cmp0 = (in0 < in1);
+    assign cmp1 = (in1 < in2);
+    assign cmp2 = (in2 < in0);
 
     always @(posedge clk) begin
-        //STAGE 0
-        rin0 <= in0;
-        rin1 <= in1;
-        rin2 <= in2;
-
-        cmp0 <= (in0 < in1);
-        cmp1 <= (in1 < in2);
-        cmp2 <= (in0 < in2);
-
-        //STAGE 1
-
         if (cmp0) begin
-            if (cmp2) min <= rin0;
-            else min <= rin2;
-
-            if (cmp1) begin
-                mid <= rin1;
-                max <= rin2;
+            if (cmp2) begin
+                min <= in2;
+                mid <= in0;
+                max <= in1;
             end
             else begin
-                mid <= rin2;
-                max <= rin1;
+                min <= in0;
+                if (cmp1) begin
+                    max <= in2;
+                    mid <= in1;
+                end
+                else begin
+                    max <= in1;
+                    mid <= in2;
+                end
             end
         end
         else begin
-            if (cmp1) min = rin1;
-            else min = rin2;
-
-            if (cmp2) begin
-                mid <= rin0;
-                max <= rin2;
+            if (cmp1) begin
+                min <= in1;
+                if (cmp2) begin
+                    mid <= in2;
+                    max <= in0;
+                end
+                else begin
+                    mid <= in0;
+                    max <= in2;
+                end
             end
             else begin
-                mid <= rin2;
-                max <= rin0;
+                min <= in2;
+                mid <= in1;
+                max <= in0;
             end
         end
+    end 
 
-    end
-
-
-    // always@(posedge clk) begin
-    //     min <= (in0 < in1) ? ((in0 < in2) ? in0 : in2) : ((in1 < in2) ? in1 : in2);
-    //     max <= (in0 > in1) ? ((in0 > in2) ? in0 : in2) : ((in1 > in2) ? in1 : in2);
-    //     mid <= (in0 > in1) ? 
-    //            ((in1 > in2) ? in1 : ((in0 > in2) ? in2 : in0)) : 
-    //            ((in0 > in2) ? in0 : ((in1 > in2) ? in2 : in1));
-    // end
-
-    
 endmodule
